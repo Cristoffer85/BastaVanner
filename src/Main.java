4503e8cf87768ba3993411a6cd3a16fb73306796
Cristoffer <119351534+Cristoffer85@users.noreply.github.com>
2023-03-14 13:03:04 +0100

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//Du har en lista med vänner. Du vill skriva ut namnen på de som är bästa vänner. (Kalle och Lisa)
+import java.util.Arrays;
+public class bastaVannen {
+    public static void main(String[] args) {
+
+        String[] vänner = { "Lisa", "Erik", "Anna", "Kajsa", "Kalle"};
+        System.out.println(vänner[0] + ", " + vänner[4]);
+
+    }
+}
